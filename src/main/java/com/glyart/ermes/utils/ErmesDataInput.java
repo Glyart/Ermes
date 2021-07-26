7602f53518199a5c0cc0186bee00a62f9d27c3e8
Aerora01 <46382079+Aerora01@users.noreply.github.com>
2021-07-27 01:03:16 +0200

    The ErmesData classes now extend the functionalities of the Google ByteArrayData classes.

--- src/main/java/com/glyart/ermes/utils/ErmesDataInput.java
+++ src/main/java/com/glyart/ermes/utils/ErmesDataInput.java
@@ -1,77 +1,97 @@
 package com.glyart.ermes.utils;
 
 import com.glyart.ermes.messages.MessageSerializable;
+import com.google.common.base.Preconditions;
 import com.google.common.io.ByteArrayDataInput;
 import com.google.common.io.ByteStreams;
 
 import java.lang.reflect.InvocationTargetException;
 import java.util.UUID;
 
-public class ErmesDataInput {
+public class ErmesDataInput implements ByteArrayDataInput {
 
     private final ByteArrayDataInput input;
 
     public ErmesDataInput(byte[] data) {
         this.input = ByteStreams.newDataInput(data);
-        
     }
 
+    public ErmesDataInput(ByteArrayDataInput input) {
+        Preconditions.checkNotNull(input);
+        this.input = input;
+    }
+
+    @Override
     public void readFully(byte[] buffer) {
         input.readFully(buffer);
     }
 
+    @Override
     public void readFully(byte[] buffer, int offset, int length) {
         input.readFully(buffer, offset, length);
     }
 
+    @Override
     public int skipBytes(int n) {
         return input.skipBytes(n);
     }
 
+    @Override
     public boolean readBoolean() {
         return input.readBoolean();
     }
 
+    @Override
     public byte readByte() {
         return input.readByte();
     }
 
+    @Override
     public int readUnsignedByte() {
         return input.readUnsignedByte();
     }
 
+    @Override
     public short readShort() {
         return input.readShort();
     }
 
+    @Override
     public int readUnsignedShort() {
         return input.readUnsignedShort();
     }
 
+    @Override
     public char readChar() {
         return input.readChar();
     }
 
+    @Override
     public int readInt() {
         return input.readInt();
     }
 
+    @Override
     public long readLong() {
         return input.readLong();
     }
 
+    @Override
     public float readFloat() {
         return input.readFloat();
     }
 
+    @Override
     public double readDouble() {
         return input.readDouble();
     }
 
+    @Override
     public String readLine() {
         return input.readLine();
     }
 
+    @Override
     public String readUTF() {
         return input.readUTF();
     }
--- src/main/java/com/glyart/ermes/utils/ErmesDataOutput.java
+++ src/main/java/com/glyart/ermes/utils/ErmesDataOutput.java
@@ -1,12 +1,13 @@
 package com.glyart.ermes.utils;
 
 import com.glyart.ermes.messages.MessageSerializable;
+import com.google.common.base.Preconditions;
 import com.google.common.io.ByteArrayDataOutput;
 import com.google.common.io.ByteStreams;
 
 import java.util.UUID;
 
-public class ErmesDataOutput  {
+public class ErmesDataOutput implements ByteArrayDataOutput {
 
     private final ByteArrayDataOutput output;
 
@@ -14,24 +15,44 @@ public class ErmesDataOutput  {
         this.output = ByteStreams.newDataOutput();
     }
 
+    public ErmesDataOutput(ByteArrayDataOutput output) {
+        Preconditions.checkNotNull(output);
+        this.output = output;
+    }
+
+    @Override
     public void write(int value) {
         output.write(value);
     }
 
+    @Override
     public void write(byte[] value) {
         output.write(value);
     }
 
+    @Override
     public void write(byte[] value, int offset, int length) {
         output.write(value, offset, length);
     }
 
+    @Override
     public void writeBoolean(boolean value) {
         output.writeBoolean(value);
     }
 
+    @Override
     public void writeByte(int value) {
         output.writeByte(value);
+    }
+
+    @Override
+    public void writeShort(int value) {
+        output.writeShort(value);
+    }
+
+    @Override
+    public void writeChar(int value) {
+        output.writeShort(value);
     }
 
     public void writeShort(short value) {
@@ -42,28 +63,39 @@ public class ErmesDataOutput  {
         output.writeChar(value);
     }
 
+    @Override
     public void writeInt(int value) {
         output.writeInt(value);
     }
 
+    @Override
     public void writeLong(long value) {
         output.writeLong(value);
     }
 
+    @Override
     public void writeFloat(float value) {
         output.writeFloat(value);
     }
+    @Override
 
     public void writeDouble(double value) {
         output.writeDouble(value);
     }
 
+    @Override
     public void writeChars(String value) {
         output.writeChars(value);
     }
 
+    @Override
     public void writeUTF(String value) {
         output.writeUTF(value);
+    }
+
+    @Override
+    public void writeBytes(String value) {
+        output.write(value.getBytes());
     }
 
     public void writeUUID(UUID uuid) {
